0e41f1b06ed46a65b4187d01cbd7064731b24a2f
Ramzi Hadrich <205276@edu.p.lodz.pl>
2018-08-07 21:14:46 +0200

    optimization and simplification

--- dbrprop.py
+++ dbrprop.py
@@ -2,15 +2,11 @@ import numpy as np
 
 class DBRprop:
     def __init__(self):
-        self.nbr = np.random.randint(15,5000)
+        self.nbr = np.random.randint(15, 5000)
         self.n = np.random.rand(2)*5 + 0.00000001
-        self.d = np.random.rand(2)*10000+ 3
+        self.d = np.random.rand(2)*10000 + 3
         # creation of array of data representing the mirror
-        self.data = np.array([[self.n[0], 0]])
-        self.data = np.vstack((self.data, [self.n[1], self.d[1]]))
-        for k in range(self.nbr - 4):
-            self.data = np.vstack((self.data, [self.n[0], self.d[0]]))
-            self.data = np.vstack((self.data, [self.n[1], self.d[1]]))
-        self.data = np.vstack((self.data, [self.n[0], self.d[0]]))
-        self.data = np.vstack((self.data, [self.n[1],0]))
+        self.data = np.tile(np.array([[self.n[0], self.d[0]], [self.n[1], self.d[1]]]), (self.nbr, 1))
+        self.data[0, 1] = 0
+        self.data[-1, 1] = 0
 
