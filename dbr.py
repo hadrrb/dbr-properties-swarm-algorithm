ced685b61039dfc659dc43edd42cdd4ebfd81b34
Ramzi Hadrich <205276@edu.p.lodz.pl>
2018-08-16 21:05:40 +0200

    GDD calculation algorithm rewritten, but still can't achieve the given example. The differentiation has be tested both with prebuilt procedure and with code written form scratch. R is calculated correctly

--- dbr.py
+++ dbr.py
@@ -1,61 +1,71 @@
-from numpy import *
-from pylab import *
+import numpy as np
+import pylab as pl
 import timeit
 import dbrprop
 
+
 class DBRMirror:
-    def __init__(self, data):
+    def __init__(self, data, incidentwavelength):
         if isinstance(data, str):
-            data = loadtxt(data)
+            data = np.loadtxt(data)
         self.n = data[:, 0]
-        self.d = array(concatenate(([0], data[1:-1, 1], [0])))
+        self.d = np.array(np.concatenate(([0], data[1:-1, 1], [0])))
+        self.r = self.reflection_coeff(incidentwavelength)
+        self.R = np.abs(self.r)**2
+        self.GD = (self.r.imag * self.diff_omega(self.r.real, incidentwavelength) - self.r.real * self.diff_omega(self.r.imag, incidentwavelength))/self.R
+        self.GDD = self.diff_omega(self.GD, incidentwavelength)
 
     def reflection_coeff(self, incidentwavelength):
         nm = (self.n[:-1] / self.n[1:])
-        R = array([])
+        r = np.empty(incidentwavelength.size) + 0j
+        i = 0
         for wavelength in incidentwavelength:
-            phi = 2j * pi * self.n[:-1] * self.d[:-1] / wavelength
-            transition_matrix = array([[0.5 * (1 + nm) * exp(-phi), 0.5 * (1 - nm) * exp(phi)],
-                                       [0.5 * (1 - nm) * exp(-phi), 0.5 * (1 + nm) * exp(phi)]])
+            phi = 2j * np.pi * self.n[:-1] * self.d[:-1] / wavelength
+            transition_matrix = np.array([[0.5 * (1 + nm) * np.exp(-phi), 0.5 * (1 - nm) * np.exp(phi)],
+                                       [0.5 * (1 - nm) * np.exp(-phi), 0.5 * (1 + nm) * np.exp(phi)]])
 
             general_transition_matrix = 1
             for k in range(self.n.size - 2, -1, -1):
-                general_transition_matrix = dot(general_transition_matrix, transition_matrix[:, :, k])
+                general_transition_matrix = np.dot(general_transition_matrix, transition_matrix[:, :, k])
 
-            r = - general_transition_matrix.item(2) / general_transition_matrix.item(3)
-            # R = append(R, abs(r)**2)
-            R = append(R, r)
+            r[i] = - general_transition_matrix.item(2) / general_transition_matrix.item(3)
+            i += 1
+        return r
 
-        phi = arctan(imag(R) / real(R))
+    def diff_omega(self, y, incidentwavelength):
         c = 29.9792458  # in nm/fs
-        GDD = incidentwavelength ** 2 / (2 * pi * c) * gradient(incidentwavelength ** 2 / (2 * pi * c) * gradient(phi))
-        return GDD
+        return (-incidentwavelength ** 2 / (2 * np.pi * c)) * np.gradient(y, incidentwavelength)
+
+    # def diff_omega(self, y, incidentwavelength):
+    #     res = np.zeros_like(y)
+    #     res[1] = ((y[2] - y[1])/(incidentwavelength[2] - incidentwavelength[1]))
+    #     res[-1] = ((y[-1] - y[-2])/(incidentwavelength[-1] - incidentwavelength[-2]))
+    #     for n in range (2, y.__len__()-1):
+    #         res[n] = (((y[n] - y[n-1]) / (incidentwavelength[n] - incidentwavelength[n-1]))
+    #         + ((y[n+1] - y[n]) / (incidentwavelength[n+1] - incidentwavelength[n])))
+    #     c = 29.9792458  # in nm/fs
+    #     return - incidentwavelength ** 2 / (2 * np.pi * c) * res
 
 
 start = timeit.default_timer()
-
-# data = array([
-#     [1.0, inf],
-#     [3.5, 74.0],
-#     [3.0, 92.5],
-#     [3.5, 74.0],
-#     [3.0, 92.5],
-#     [3.5, 74.0],
-#     [3.0, 92.5],
-#     [3.5, inf]
-# ])
-
-New = dbrprop.DBRprop()
-NewMirror = DBRMirror(New.data)
-
-wavelength = array(linspace(400, 4000, 1000))
-
-gdd = NewMirror.reflection_coeff(wavelength)
+wavelength = np.linspace(800, 1200, 1000)
+NewMirror = DBRMirror("dbr.txt", wavelength)
 
 stop = timeit.default_timer()
 print(stop - start)
 
-plot(wavelength, gdd)
-xlabel("$\lambda$")
-ylabel("GDD [fs**2]")
-show()
+pl.figure()
+pl.plot(wavelength, NewMirror.R)
+pl.xlabel("$\lambda$")
+pl.ylabel("R")
+pl.xlim((950,1150))
+pl.show(block=False)
+
+pl.figure()
+pl.plot(wavelength, NewMirror.GDD)
+pl.xlabel("$\lambda$")
+pl.ylabel("GDD [fs**2]")
+pl.xlim((1000,1080))
+pl.show()
+
+
